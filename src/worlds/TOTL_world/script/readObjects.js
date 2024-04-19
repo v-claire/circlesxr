8a8fce54be8e47c53166083a0bb6032a7e06cbd4
v-claire <claire.vail24@gmail.com>
2024-04-18 20:23:18 -0400

    json read

---
+++ src/worlds/TOTL_world/script/readObjects.js
@@ -0,0 +1,65 @@
+function fetchJSONData() {
+    fetch("script/assetList.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(function(res) {
+            
+            //console.log(res);
+            createInfo(res);
+
+        })
+              
+}
+
+window.onload = (event) => {
+    fetchJSONData();
+};      
+
+
+function createInfo(data) {
+    let objArray = data;
+    let size = objArray.items.length;
+
+    for (let i = 0; i < size; i++) {
+        //console.log(objArray.items[i]);
+        let itemID = objArray.items[i].tagID;
+        let itemObj = document.getElementById(itemID);
+
+        if (itemObj) {
+            if (!itemObj.hasAttribute("circles-artefact")) {
+                itemObj.setAttribute("circles-artefact", "");
+            }
+
+            let itemTitle = objArray.items[i].info.name;
+            let itemYear = toString(objArray.items[i].info.year);
+            let itemDesc = objArray.items[i].info.desc;
+            let itemNum = objArray.items[i].info.idNum;
+
+            itemObj.setAttribute("circles-artefact",
+            "title: " + itemTitle + ";" +
+            "description: " + itemDesc + "\n" + 
+            "Year: " + itemYear + "\n" +
+            "Artifact ID: " + itemNum + ";" +
+            "descriptionLookAt: true;");
+        }
+
+
+
+
+
+        //console.log(itemID);
+    }
+    //console.log(size);
+   // console.log(objArray.items[0]);
+
+
+
+    //console.log(objArray);
+    //for 
+    
+}
